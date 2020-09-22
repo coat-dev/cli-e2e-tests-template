78133ff64c11689553cee71a4c78e4b6e9c373ea
Peter Juras <me@peterjuras.com>
2020-09-22 07:38:56 +0200

    chore: :bookmark: @coat/e2e-test-template-sync-5 v1.0.0

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: "@coat/e2e-test-template-sync-4",
+  name: "@coat/e2e-test-template-sync-5",
   extends: ["@coat/e2e-test-template"],
   files: [
     {
-      file: "e2e-test-template-sync-4",
+      file: "e2e-test-template-sync-5",
       content: {
         b: 1,
       },
