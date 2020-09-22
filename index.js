db95dffa39bc88652919771de9540738c29ff81b
Peter Juras <me@peterjuras.com>
2020-09-22 07:43:45 +0200

    chore: :bookmark: @coat/e2e-test-template-run-1 v1.0.0

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 module.exports = {
-  name: "@coat/e2e-test-template",
+  name: "@coat/e2e-test-template-run-1",
+  scripts: [
+    {
+      id: "test-script-1",
+      scriptName: "test-script",
+      run: "echo Running test script 1",
+    },
+    {
+      id: "test-script-2",
+      scriptName: "test-script",
+      run: "echo Running test script 2",
+    },
+    {
+      id: "test-script-3",
+      scriptName: "another-test-script",
+      run: "echo Running another test script",
+    },
+  ],
 };
